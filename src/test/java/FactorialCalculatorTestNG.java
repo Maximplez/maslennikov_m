dd1b3bc3c898d1642318d44bbf2278a0a4643307
Maximplez <borderlands99999@gmail.com>
2024-11-12 01:32:11 +0700

    Add files via upload

---
+++ src/test/java/FactorialCalculatorTestNG.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+import org.example.FactorialCalculator;
+public class FactorialCalculatorTestNG {
+
+    @Test
+    public void testFactorial() {
+        assertEquals(FactorialCalculator.factorial(0), 1);
+        assertEquals(FactorialCalculator.factorial(1), 1);
+        assertEquals(FactorialCalculator.factorial(5), 120);
+        assertEquals(FactorialCalculator.factorial(6), 720);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Число должно быть неотрицательным.")
+    public void testFactorialNegative() {
+        FactorialCalculator.factorial(-1);
+    }
+}
